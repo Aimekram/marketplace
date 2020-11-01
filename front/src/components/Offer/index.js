0b9452be2eff36fc6bb01466a9b17f52b76731a7
Aimekram <janina.byszewska@gmail.com>
2020-11-01 17:01:05 +0100

    Display offer details in single offer view

--- front/src/components/Offer/index.js
+++ front/src/components/Offer/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from 'react-router-dom';
+
 import { BASE_URL } from "../../constants";
+import Photo from "../../images/dragon.jpg";
+import { List } from "../../styledComponents/StyledLists";
 
 const Offer = ({ location }) => {
     const query = new URLSearchParams(location.search);
@@ -24,11 +27,40 @@ const Offer = ({ location }) => {
       fetchData();
   
       return () => { abortController.abort() };
-    }, []);
+    }, [offerId]);
 
+    const { processor, graphics, description, loc, price } = offers || {}
 
-    console.log(offers)
-return <div>Full Offer id: {offerId}</div>
+   
+    return (
+        <article>
+        {!!processor ?
+            <>
+                <img src={Photo} alt=""/>
+                <p>{description}</p>
+                <List>
+                    <li>
+                        <h3>Procesor:</h3>
+                        <span>{processor.name}</span>
+                    </li>
+                    <li>
+                        <h3>Karta graficzna:</h3>
+                        <span>{graphics.Bus}</span>
+                    </li>
+                    <li>
+                        <h3>Cena:</h3>
+                        <span>{price} PLN</span>
+                    </li>
+                    <li>
+                        <h3>Lokalizacja:</h3>
+                        <span>{loc}</span>
+                    </li>
+                </List>
+            </>
+            : <p>Wczytywanie szczegółów ogłoszenia...</p>
+        }
+    </article>
+    )
 }
 
 export default withRouter(Offer);
