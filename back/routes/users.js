1a41f9c91e6296ac027a77bbae5bade906351da0
Aimekram <janina.byszewska@gmail.com>
2021-03-21 21:40:30 +0100

    Hash passwords and add /login route

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const { User } = require('../models');
 
@@ -40,20 +41,52 @@ router.get('/:id', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
 	console.log(req.body);
+	const { email: userEmail, password: userPassword } = req.body;
+
 	let user = await User.findOne({
-		email: req.body.email,
+		email: userEmail,
 	});
 	if (user) {
 		return res
 			.status(400)
-			.send('Wygląda na to, że masz już u nas konto - zaloguj się'); //TODO: make this message appear on front
+			.send('Wygląda na to, że masz już u nas konto - zaloguj się'); //TODO: make this message appear on front?
 	}
-	user = new User({
-		email: req.body.email,
-		password: req.body.password,
+	try {
+		const hashedPassword = await bcrypt.hash(userPassword, 10);
+		user = new User({
+			email: userEmail,
+			password: hashedPassword,
+		});
+		user.save();
+		res.status(201).json(user);
+	} catch {
+		res.status(500).send(); //TODO: what's correct status here?
+	}
+});
+
+/**
+ * @method - POST
+ * @param - /login
+ * @description - user login
+ */
+
+router.post('/login', async (req, res) => {
+	const { email: userEmail, password: userPassword } = req.body;
+	const user = await User.findOne({
+		email: userEmail,
 	});
-	user.save();
-	res.status(200).json(user);
+	if (user === null) {
+		return res.status(400).send('Cannot find user');
+	}
+	try {
+		if (await bcrypt.compare(userPassword, user.password)) {
+			res.send('Success');
+		} else {
+			res.send('Not allowed');
+		}
+	} catch {
+		res.status(500).send('Error'); //TODO: what's correct status here?
+	}
 });
 
 module.exports = router;
