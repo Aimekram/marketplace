b3c2359f914db3cc698e467283c49e7105f6c6b6
Aimekram <janina.byszewska@gmail.com>
2021-03-21 18:46:30 +0100

    Controll form elements and add some description

--- front/src/components/Signup/index.js
+++ front/src/components/Signup/index.js
@@ -1,43 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// TODO: 1. hash password 1b. email confirmation 2. what's next - thank you message? 3. Hide navigation - minimalize distraction
 
 const Signup = () => {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    let data = new FormData(event.target);
-    let body = {};
-    data.forEach((value, property) => (body[property] = value));
+	// controll form elements
+	const [formValues, setFormValues] = useState({
+		email: '',
+		password: '',
+	});
 
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    };
-    console.log(requestOptions.body);
+	const handleChange = (event) =>
+		setFormValues({
+			...formValues,
+			[event.target.name]: event.target.value,
+		});
 
-    try {
-      await fetch(`http://localhost:4000/api/users/signup`, requestOptions);
-    } catch (error) {
-      console.log(error);
-    }
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(formValues),
+		};
+		console.log(requestOptions.body);
 
-    // clear data
-    data = new FormData();
-    body = {};
-  };
+		try {
+			await fetch(
+				`http://localhost:4000/api/users/signup`,
+				requestOptions
+			);
+			// TODO: delete clear inputs - not needed if redirecting
+			event.target.reset();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <form onSubmit={handleSubmit} method='post'>
-      <label>
-        Name:
-        <input type='text' name='name' />
-      </label>
-      <label>
-        Password:
-        <input type='password' name='password' />
-      </label>
-      <input type='submit' value='wyślij' />
-    </form>
-  );
+	return (
+		<div>
+			<h1>LOGO</h1>
+			<h2>
+				Stwórz konto w naszym serwsie, by móc wystawiać ogłoszenia z
+				super komputerami // podać dobre uzasadnienie
+			</h2>
+			<form onSubmit={handleSubmit} method='post'>
+				<label>
+					Email:
+					<input
+						type='email'
+						name='email'
+						placeholder='Adres e-mail'
+						value={formValues.email}
+						onChange={handleChange}
+					/>
+				</label>
+				<label>
+					Hasło:
+					<input
+						type='password'
+						name='password'
+						placeholder='Hasło'
+						vaue={formValues.password}
+						onChange={handleChange}
+					/>
+				</label>
+				<input type='submit' value='Wyślij' />
+			</form>
+			<p>
+				Masz już konto? <a href='/login'>Zaloguj się</a>
+			</p>
+		</div>
+	);
 };
 
 export default Signup;
